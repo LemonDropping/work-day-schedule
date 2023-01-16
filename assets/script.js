b13e95ab6efcdc67acd0268bac8fed168035ed30
Cole Roberts <coleroberts76@gmail.com>
2023-01-16 15:54:45 -0600

    created current time and date appear on page

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var timeDisplay = dayjs()n
+const currentDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
+const currentHour = dayjs().format('HH');
+$('#currentDay').text(currentDate);
+console.log(currentDate);
 
-$(function () {
+
+
+    
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -23,4 +28,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
